a698a20327a7e6085a64487ea1007e6423ed7b0d
gxjin <schach@qq.com>
2024-10-19 00:45:31 +0200

    tmp

--- download_lotss_dr3.py
+++ download_lotss_dr3.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-    Check and download all the MaNGA DAP LOGCUBE files to the given path.
+    Check and download all the LoTSS DR3 images and rms images which
+    contain the MaNGA galaxies.
     
     Copyright (C) 2019-2024, Gaoxiang Jin
 
     E-mail: gx-jin@outlook.com
-
-    Updated versions of the codes are available from github pages:
-    https://github.com/gx-jin/py_astro_gxjin
 
     This software is provided as is without any warranty whatsoever.
     Permission to use, for non-commercial purposes is granted.
@@ -18,49 +16,79 @@
 """
 
 import os
-import requests
-import shutil
-from astropy.io import fits
+import numpy as np
+import pandas as pd
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
 from tqdm import tqdm
+from requests.auth import HTTPBasicAuth
+import shutil
 
 
-# def download_logcube(dapall='?', save_dir='?', daptype='SPX', test=False):
-#     if not os.path.exists(dapall):
-#         raise RuntimeError("No such directory: " + str(dapall))
-#     elif not os.path.exists(save_dir):
-#         raise RuntimeError("No such directory: " + str(save_dir))
-#     elif not test:
-#         hdu = fits.open(dapall)
-#         plate = hdu[1].data['PLATE']
-#         ifu = hdu[1].data['IFUDESIGN']
-#         save_loc = f'{save_dir}/manga-{plate[1]}-{ifu[1]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
-#         maps_url = f'https://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/{daptype}-MILESHC-MASTARSSP/{plate[1]}/{ifu[1]}/manga-{plate[1]}-{ifu[1]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
-#         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; \
-#                 rv:80.0) Gecko/20100101 Firefox/80.0'}
-#         with requests.get(maps_url, headers=headers, stream=True) as r:
-#             if r.status_code == requests.codes.ok:
-#                 with open(save_loc, 'wb') as f:
-#                     shutil.copyfileobj(r.raw, f)
-#     else:
-#         hdu = fits.open(dapall)
-#         plate = hdu[1].data['PLATE']
-#         ifu = hdu[1].data['IFUDESIGN']
-#         for i in tqdm(range(len(plate))):  
-#             save_loc = f'{save_dir}/manga-{plate[i]}-{ifu[i]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
-#             if os.path.exists(save_loc):
-#                 continue
-#             else:
-#                 maps_url = f'https://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/{daptype}-MILESHC-MASTARSSP/{plate[i]}/{ifu[i]}/manga-{plate[i]}-{ifu[i]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
-#                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; \
-#                     rv:80.0) Gecko/20100101 Firefox/80.0'}
-#                 with requests.get(maps_url, headers=headers, stream=True) as r:
-#                     if r.status_code == requests.codes.ok:
-#                         with open(save_loc, 'wb') as f:
-#                             shutil.copyfileobj(r.raw, f)
+import requests
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; \
+            x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
 
-# dapall_dir = '../dap'
-# save_dir = './'
-# download_logcube(dapall=dapall_dir, save_dir=save_dir, daptype='SPX', test=True)
+dr3df = pd.read_html('/afs/mpa/home/gxjin/code-mpa/data/dr3list_241018.html')[0]
+
+fieldid = dr3df['Field name'].to_numpy()
+ra_dr3 = dr3df['RA'].to_numpy()
+dec_dr3 = dr3df['Dec'].to_numpy()
+
+cat_dap = Table.read('/afs/mpa/home/gxjin/code-mpa/data/dapall-v3_1_1-3.1.0_spx.fits')
+
+ra_dap = cat_dap['OBJRA'].data
+dec_dap = cat_dap['OBJDEC'].data
+
+mosiac_size = 2.2#1.917 # assuming mosiac size = 1.917 deg
+dr3field = np.zeros_like(ra_dap, dtype='S11')
+
+dap_skycoo = SkyCoord(ra_dap, dec_dap, frame='icrs', unit='deg')
+field_skycoo = SkyCoord(ra_dr3, dec_dr3, frame='icrs', unit='deg')
+
+for i in tqdm(range(len(ra_dap))):
+    septmp = dap_skycoo[i].separation(field_skycoo).deg
+    if np.min(septmp) < mosiac_size:
+        dr3field[i] = fieldid[septmp == np.min(septmp)][0]
+    elif np.min(septmp) >= mosiac_size:
+        dr3field[i] = 'NOCOVERAGE'
+    else:
+        print('Warning')
+
+
+def download_mosaic(fid, save_loc):
+    savefile = save_loc+fid+'_high_mosaic.fits'
+    if os.path.exists(savefile):
+        return
+    else:
+        url = f'https://lofar-surveys.org/downloads/DR3/mosaics/{fid}/mosaic-blanked.fits'
+        with requests.get(url, headers=headers, stream=True, timeout=540,
+                          auth = HTTPBasicAuth('surveys', '150megahertz')) as r:
+            if r.status_code == requests.codes.ok:
+                with open(save_loc, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+                    
+def download_rms(fid, save_loc):
+    savefile = save_loc+fid+'_rms.fits'
+    if os.path.exists(savefile):
+        return
+    else:
+        url = f'https://lofar-surveys.org/downloads/DR3/mosaics/{fid}/mosaic-blanked--final.rms.fits'
+        with requests.get(url, headers=headers, stream=True, timeout=540,
+                          auth = HTTPBasicAuth('surveys', '150megahertz')) as r:
+            if r.status_code == requests.codes.ok:
+                with open(save_loc, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+            
+        
+for i in tqdm(range(1)): #len(ra_dap)
+    fidtmp = dr3field[i].decode('utf-8')
+    if fidtmp == 'NOCOVERAGE':
+        continue
+    else:
+        download_mosaic(fidtmp, '/afs/mpa/temp/gxjin/LOTSS3/')
+        download_rms(fidtmp, '/afs/mpa/temp/gxjin/LOTSS3/')
+        
 
 # # todo: check how many files, etc.
