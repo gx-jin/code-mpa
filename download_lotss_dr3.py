64516a2b53786a2f82f7afd5243bca7db455e4ca
gxjin <schach@qq.com>
2024-10-19 23:11:30 +0200

    tmp

--- download_lotss_dr3.py
+++ download_lotss_dr3.py
@@ -63,7 +63,7 @@ def download(url: str, local_filename: str,
                 print(f"File '{local_filename}' downloaded")
             return        
         else:
-            print(f'HTTP error occurred: {response.status_code}')
+            print(f'HTTP error {response.status_code}: {url}')
     except requests.RequestException:
         print('Invalid url')
 
